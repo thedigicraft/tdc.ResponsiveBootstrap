ad135c47b15fb8ef614bc2efbc6d1da8bd6d24d9
thedigicraft <alan@rtistics.com>
2015-11-16 00:01:50 -0500

    moved

--- example.js
+++
@@ -1,8 +0,0 @@
-// Update the size monitor with current width:
-$( "#size" ).html( window.innerWidth );
-
-// On window resize:
-$( window ).resize(function() {
-  // Update the size monitor:
-  $( "#size" ).html( window.innerWidth );
-});
